83ab3befa0067631a03b3dfb96f731375efbb499
Jesus <jesusalejandroap11@gmail.com>
2021-09-04 18:11:07 -0400

    Added Codes

--- CallofDutyFriendsStats/ViewController.swift
+++ CallofDutyFriendsStats/ViewController.swift
@@ -12,6 +12,28 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        var username = "jesusalejandroap11@gmail.com"
+        var password = "Lashocker0"
+        
+        //URL
+        let url = URL(string: "https://s.activision.com/activision/login" )!
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+     
+//        guard url != nil else {
+//            print("Error creating url object")
+//        }
+        //URL REQUEST
+//        var request = URLRequest(url: <#T##URL#>, cachePolicy: <#T##URLRequest.CachePolicy#>, timeoutInterval: <#T##TimeInterval#>)
+        
+        let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
+            guard let data = data else { return }
+            print(String(data: data, encoding: .utf8)!)
+        }
+
+        task.resume()
     }
 
 
